aa61ca1a95b162cd2b62eed4de64ed6e5ed1f22d
lukeworm18 <149636404+lukeworm18@users.noreply.github.com>
2026-02-09 19:16:32 -0600

    Fix api.ts to have authentication and sign in working when tested from ExpoGo on the phone assuming you input your localhost IP where it says to.

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,15 +2,16 @@ import { Platform } from "react-native";
 
 const DEV_PORT = 3000;
 
-const ENV_URL = process.env.EXPO_PUBLIC_API_URL;
+const ENV_URL = process.env.EXPO_PUBLIC_API_BASE || process.env.EXPO_PUBLIC_API_URL;
 
 const ANDROID_EMULATOR_HOST = `http://10.0.2.2:${DEV_PORT}`;
 const IOS_SIMULATOR_HOST = `http://localhost:${DEV_PORT}`;
 
 //Fallback for physical device testing (EDIT THIS TO YOUR OWN PC IP IF YOU ARE TESTING)
-const LAN_FALLBACK_HOST = `http://192.168.4.35:${DEV_PORT}`;
+const LAN_FALLBACK_HOST = `http://<YOUR IP HERE>:${DEV_PORT}`;
 
 export const API_BASE =
   ENV_URL ||
-  (Platform.OS === "android" ? ANDROID_EMULATOR_HOST : IOS_SIMULATOR_HOST) ||
-  LAN_FALLBACK_HOST;
+  (Platform.OS === "android" ? ANDROID_EMULATOR_HOST : LAN_FALLBACK_HOST) ||
+  IOS_SIMULATOR_HOST;
+console.log("[API] API_BASE =", API_BASE);
