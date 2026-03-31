70c831815f21dc3880add745a1d3c04289128e2a
lukeworm18 <lukeworm18@gmail.com>
2026-03-30 23:03:52 -0500

    Revert "Fix missing DB columns and invalid Prisma fields"
    This reverts commit 8068d1af038faaf41d63548894b6f2b62b2c40f4.

--- server/index.js
+++ server/index.js
@@ -546,6 +546,8 @@ const energy = nutrients.find((n) =>
             imageUrl: null,
             source: "FDC_API",
             externalId: f.fdcId ? String(f.fdcId) : null,
+            servingQty: servingSize,
+            kcal: (typeof calories === "number") ? Math.round(calories) : null,
           };
         })
         .filter((x) => x.name);
